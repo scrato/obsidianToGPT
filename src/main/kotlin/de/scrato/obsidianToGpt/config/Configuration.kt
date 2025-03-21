a8c47ab03b457d2c719c81e080e82f1ff47e9cd0
scrato <Mickaello2003@gmx.de>
2025-03-21 21:24:49 +0100

    Refactor configuration to replace PathConfig with PathProperties and toggle Ngrok usage in application profiles

--- src/main/kotlin/de/scrato/obsidianToGpt/config/Configuration.kt
+++ src/main/kotlin/de/scrato/obsidianToGpt/config/Configuration.kt
@@ -1,10 +1,36 @@
 package de.scrato.obsidianToGpt.config
 
 import de.scrato.obsidianToGpt.config.properties.*
+import de.scrato.obsidianToGpt.repositories.UserRepository
+import de.scrato.obsidianToGpt.services.CustomUserDetailsService
 import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.authentication.AuthenticationProvider
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
 
 
 @Configuration
-@EnableConfigurationProperties(JwtProperties::class, ApplicationProperties::class)
-class Configuration
+@EnableConfigurationProperties(JwtProperties::class, ApplicationProperties::class, PathProperties::class)
+class Configuration {
+    @Bean
+    fun userDetailsService(userRepository: UserRepository): UserDetailsService =
+        CustomUserDetailsService(userRepository)
+    @Bean
+    fun encoder(): PasswordEncoder = BCryptPasswordEncoder()
+    @Bean
+    fun authenticationProvider(userRepository: UserRepository): AuthenticationProvider =
+        DaoAuthenticationProvider()
+            .also {
+                it.setUserDetailsService(userDetailsService(userRepository))
+                it.setPasswordEncoder(encoder())
+            }
+    @Bean
+    fun authenticationManager(config: AuthenticationConfiguration): AuthenticationManager =
+        config.authenticationManager
+}
